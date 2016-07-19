0f17c0d5cea1ee8d559cc41f52c6dcf9fb44665a
Josh Muenster <josh.muenster@nike.com>
2016-07-18 23:06:30 -0700

    fixes a few problems w/ the cfn update script

--- bin/update_stack.sh
+++ bin/update_stack.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-s3_key="$1"
-s3_bucket="$2"
-stack_name="$3"
+cfn_template="$1"
+region="$2"
+src_pkg="$3"
+s3_key="$4"
+s3_bucket="$5"
+stack_name="$6"
 
 exclude="-x *.pyc -x *test.py -x *.sh"
 update_states="UPDATE_IN_PROGRESS UPDATE_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_ROLLBACK_IN_PROGRESS UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS"
 
 echo "$( date ) creating package"
-[ -f ../${pkg_name} ] && rm ../${pkg_name}
-output=$( zip -r ../${pkg_name} ./* ${exclude} 2>&1 )
+[ -f ${src_pkg} ] && rm ${src_pkg}
+[ -f $( dirname ${src_pkg} ) ] || mkdir $( dirname ${src_pkg} )
+output=$( zip -r ${src_pkg} ./* ${exclude} 2>&1 )
 if [ "${?}" -ne 0 ]; then
   echo "errors: ${output}"
   exit 1
@@ -17,9 +21,10 @@ fi
 
 echo "$( date ) copying package to s3"
 output=$( aws s3api put-object \
+  --region ${region} \
   --bucket ${s3_bucket} \
   --key ${s3_key} \
-  --body ../${pkg_name} \
+  --body ${src_pkg} \
   --query 'VersionId' \
   --output text 2>&1 )
 if [ "${?}" -ne 0 ]; then
@@ -30,10 +35,14 @@ version_id=${output}
 
 echo "$( date ) updating cfn stack"
 output=$( aws cloudformation update-stack \
+  --region ${region} \
   --stack-name ${stack_name} \
-  --template-body file://./template.json \
+  --template-body file://${cfn_template} \
   --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=S3Version,ParameterValue=${version_id} 2>&1 )
+  --parameters ParameterKey=S3Bucket,ParameterValue=${bucket_name} \
+               ParameterKey=S3Key,ParameterValue=${funct_name}/${funct_name}-pkg.zip \
+               ParameterKey=S3Version,ParameterValue=${version_id} \
+               ParameterKey=ExecRole,ParameterValue=${lambda_exec_role} 2>&1 )
 if [ "${?}" -ne 0 ]; then
   echo "errors: ${output}"
   exit 1
@@ -42,6 +51,7 @@ fi
 echo "$( date ) waiting for cfn update"
 while [ 1 ]; do
   output=$( aws cloudformation list-stacks \
+    --region ${region} \
     --stack-status-filter ${update_states}  \
     --query 'StackSummaries[?StackName==`'${stack_name}'`].StackName' \
     --output text 2>&1 )
@@ -52,6 +62,7 @@ while [ 1 ]; do
     sleep 5
   else
     stack_state= $( aws cloudformation list-stacks \
+      --region ${region} \
       --stack-status-filter ${update_states}  \
       --query 'StackSummaries[?StackName==`'${stack_name}'`].StackName' \
       --output text 2>&1 )
